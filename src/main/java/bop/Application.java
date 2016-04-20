185f65a6c7c07809bdeb75deb7f771f1af95e10c
Anatoly Yashkin <mail@anatolko.info>
2016-04-20 21:30:48 +0300

    Исправил апи для получения информации о текущем пользователе

--- src/main/java/bop/Application.java
+++ src/main/java/bop/Application.java
@@ -1,18 +1,9 @@
 package bop;
 
-/**
- * Created by Stretch on 19.05.2015.
- */
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableJpaRepositories
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
